0a3d5bae16446d44626927ec0243b9555f891242
Issam BELKADA <belkadaissam@gmail.com>
2026-03-31 19:45:56 +0100

    frontend setuo react + vite + tailwindcss

---
+++ fleet_frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="bg-red-500">
+      Hello
+      <div className="text-3xl font-bold underline">
+        World
+      </div>
+    </div>
+  )
+}
+
+export default App
